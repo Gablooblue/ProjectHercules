e772281e6f2406b8f47e57ec86c55ac61919766b
gablooblue <gabrielhenry.lopez@gmail.com>
2017-02-04 17:30:35 +0800

    BMREquation class changed

--- ProjectHercules/app/src/main/java/com/sendnodes/projecthercules/BMREquation.java
+++ ProjectHercules/app/src/main/java/com/sendnodes/projecthercules/BMREquation.java
@@ -2,13 +2,14 @@
 
 public class BMREquation
 {
-    static double BMR;
+    private static double BMR;
     /*
     * @params height, weight, age, gender
     * @returns calculated BMR
     */
-    public static float calculator(int height, float weight, int age, char gender)
+    public static double calculator(int height, float weight, int age, char gender)
     {
+        //gender is inputted as M or F
         if(gender == 'M')
         {
             BMR = (10 * weight + 6.25 * height - 5 * age + 5);
